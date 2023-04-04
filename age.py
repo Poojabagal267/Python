f87e7e6b942ece1a3ad76aee5a214f9a81d7ab42
Poojabagal267 <99575872+Poojabagal267@users.noreply.github.com>
2023-04-04 19:56:44 +0530

    Update age.py

--- age.py
+++ age.py
@@ -1,3 +1,9 @@
+"""Write a Python program that asks the user to enter their age
+ and then prints "You are a minor" if their age is less than 18, 
+"You are an adult" if their age is greater than or equal to 18 and less than 65, and
+ "You are a senior" if their age is 65 or greater
+"""
+
 age=int(input("Enter the age:"))
 if age<18:
   print("You are a minor.") 
