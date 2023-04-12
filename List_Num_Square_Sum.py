f5e698da4dedf9601ee8b64abe8c2e261034082b
Poojabagal267 <99575872+Poojabagal267@users.noreply.github.com>
2023-04-12 10:01:29 +0530

    Create List_Num_Square_Sum.py

---
+++ List_Num_Square_Sum.py
@@ -0,0 +1,8 @@
+"""Write a Python function that takes in a list of numbers and
+ returns the sum of the squares of all the numbers in the list.""l
+list = [2,5,6,9,4,1,8,12];
+sum=0;
+for num in list:
+  square=(num*num);
+  sum+=square;
+print("Sum is ",sum);
